a6ee2a342053397d3579b349b08334fa2f7c5c8e
Anna Bodily <annalallen6@gmail.com>
2024-03-21 15:54:19 -0600

    okay now it's really pretty!

--- Project Background Analysis.R
+++ Project Background Analysis.R
@@ -29,6 +29,36 @@ subdata$discuss_topic3 <- as.logical(subdata$discuss_topic3)
 subdata$discuss_topic4 <- as.logical(subdata$discuss_topic4)
 subdata$discuss_topic5 <- as.logical(subdata$discuss_topic5)
 subdata$discuss_topic6 <- as.logical(subdata$discuss_topic6)
+subdata$premarital <- as.logical(subdata$premarital)
+
+# Reorder certain variable factor levels:
+inc_order <- c("Under $5000", "$5000-$7499", "$7500-$9999", "$10,000-$12,499", "$12,500-$14,999", "$15,000-$19,999", 
+"$20,000-$24,999", "$25,000-$29,999", "$30,000-$34,999", "$35,000-$39,999", "$40,000-$49,999", "$50,000-$59,999", "$60,000-$74,999",
+"$75,000-$99,999", "$100,000 or more")
+subdata$TOTINCR <- factor(subdata$TOTINCR, levels = inc_order)
+
+race_order <- c("WHITE", "BLACK", "OTHER")
+subdata$RACE <- factor(subdata$RACE, levels = race_order)
+
+parmar_order <- c("Yes", "No", "Don't Know", "Refused")
+subdata$PARMARR <- factor(subdata$PARMARR, levels = parmar_order)
+
+intact_order <- c("Yes", "No", "Don't Know")
+subdata$INTACT18 <- factor(subdata$INTACT18, levels = intact_order)
+
+educ_order <- c("9TH GRADE OR LESS", "10TH GRADE", "11TH GRADE", "12TH GRADE, NO DIPLOMA (NOR GED)", "HIGH SCHOOL GRADUATE (DIPLOMA OR GED)",
+"SOME COLLEGE BUT NO DEGREE", "ASSOCIATE DEGREE IN COLLEGE/UNIVERSITY", "BACHELOR'S DEGREE", "MASTER'S DEGREE", "DOCTORATE DEGREE")
+subdata$HIEDUC <- factor(subdata$HIEDUC, levels = educ_order)
+
+rel_order <- c("Catholic", "Baptist/Southern Baptist", "Methodist, Lutheran, Presbyterian, Episcopal", "Fundamentalist Protestant",
+"Other Protestant denomination","Protestant - No specific denomination", "Other religion", "No religion", "Don't know", "Refused")
+subdata$RELRAISD <- factor(subdata$RELRAISD, levels = rel_order)
+
+chufreq_order <- c("More than once a week", "Once a week", "2-3 times a month", "Once a month (about 12 times a year)", "3-11 times a year", "Once or twice a year",
+"Never", "Refused", "Missing")
+subdata$ATTND14 <- factor(subdata$ATTND14, levels = chufreq_order)
+
+
 
 # Label variables:
 label(subdata$AGER) <- "Age at Time of Survey"
@@ -50,7 +80,8 @@ label(subdata$discuss_topic3) <- "Taught Where to Get Birth Control"
 label(subdata$discuss_topic4) <- "Taught About Sexually Transmitted Diseases"
 label(subdata$discuss_topic5) <- "Taught How to Prevent HIV/AIDS"
 label(subdata$discuss_topic6) <- "Taught How to Use a Condom"
+label(subdata$premarital) <- "Had Premarital Sex"
 
 # Actually build the Table 1:
-my_table <- table1(~ AGER + RACE + PARMARR + INTACT18 + TOTINCR + HIEDUC + RELRAISD + ATTND14 + RELIGION + EVRMARRY + ONOWN18 + VRY1STAG + topics_discussed_count + discuss_topic1 +
+my_table <- table1(~ AGER + RACE + PARMARR + INTACT18 + TOTINCR + HIEDUC + RELRAISD + ATTND14 + EVRMARRY + ONOWN18 + VRY1STAG + topics_discussed_count + discuss_topic1 +
 discuss_topic2 + discuss_topic3 + discuss_topic4 + discuss_topic5 + discuss_topic6 | discuss_topic8, data = subdata, render = rndr, render.continuous=c("Mean (SD)"="MEAN (SD)"))

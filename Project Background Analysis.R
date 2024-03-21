4343345c135d07ce879bc4d0e8c10c0092274202
Anna Bodily <annalallen6@gmail.com>
2024-03-21 15:07:23 -0600

    labelling finalized (hopefully?)

--- Project Background Analysis.R
+++ Project Background Analysis.R
@@ -15,38 +15,42 @@ discuss_topic2, discuss_topic3, discuss_topic4, discuss_topic5, discuss_topic6, 
 # Remove the null values from premarital (due to missing answers):
 subdata <- subdata[!is.na(subdata$premarital), ]
 
-table(subdata$discuss_topic6)
+# Setting up a render function for logical variables to save space:
+rndr <- function(x, ...) {
+  y <- render.default(x, ...)
+  if (is.logical(x)) y[2] else y
+}
 
-# Make variables into factors as necessary:
+# Make variables into factors/logicals as necessary:
 subdata$discuss_topic8 <- factor(subdata$discuss_topic8, levels = c(0,1), labels = c("Taught to Abstain From PS", "Not Taught to Abstain From PS"))
-subdata$discuss_topic1 <- factor(subdata$discuss_topic1, levels = c(0,1), labels = c("No", "Yes"))
-subdata$discuss_topic2 <- factor(subdata$discuss_topic2, levels = c(0,1), labels = c("No", "Yes"))
-subdata$discuss_topic3 <- factor(subdata$discuss_topic3, levels = c(0,1), labels = c("No", "Yes"))
-subdata$discuss_topic4 <- factor(subdata$discuss_topic4, levels = c(0,1), labels = c("No", "Yes"))
-subdata$discuss_topic5 <- factor(subdata$discuss_topic5, levels = c(0,1), labels = c("No", "Yes"))
-subdata$discuss_topic6 <- factor(subdata$discuss_topic6, levels = c(0,1), labels = c("No", "Yes"))
+subdata$discuss_topic1 <- as.logical(subdata$discuss_topic1)
+subdata$discuss_topic2 <- as.logical(subdata$discuss_topic2)
+subdata$discuss_topic3 <- as.logical(subdata$discuss_topic3)
+subdata$discuss_topic4 <- as.logical(subdata$discuss_topic4)
+subdata$discuss_topic5 <- as.logical(subdata$discuss_topic5)
+subdata$discuss_topic6 <- as.logical(subdata$discuss_topic6)
 
 # Label variables:
-label(table$AGER) <- "Age"
-label(table$RACE) <- "Race"
-label(table$PARMARR) <- "Parents Married"
-label(table$INTACT18) <- "Lived in Intact Family From 0-18"
-label(table$TOTINCR) <- "Family Income"
-label(table$HIEDUC) <- "Highest Level of Education Achieved"
-label(table$RELRAISD) <- "Religion Raised In"
-label(table$ATTND14) <- "Frequency of Church Attendance at 14"
-label(table$RELIGION) <- "Current Religious Affiliation"
-label(table$EVRMARRY) <- "Ever Been Married"
-label(table$ONOWN18) <- "Lived on Their Own Before 18"
-label(table$VRY1STAG) <- "Age at First Intercourse"
-label(table$topics_discussed_count) <- "How Many Sexual Topics Taught"
-label(table$discuss_topic1) <- "Taught How to Say No to Sex"
-label(table$discuss_topic2) <- "Taught Methods of Birth Control"
-label(table$discuss_topic3) <- "Taught Where to Get Birth Control"
-label(table$discuss_topic4) <- "Taught About Sexually Transmitted Diseases"
-label(table$discuss_topic5) <- "Taught How to Prevent HIV/AIDS"
-label(table$discuss_topic6) <- "Taught How to Use a Condom"
+label(subdata$AGER) <- "Age at Time of Survey"
+label(subdata$RACE) <- "Race"
+label(subdata$PARMARR) <- "Parents Married at Birth"
+label(subdata$INTACT18) <- "Lived in Intact Family From 0-18"
+label(subdata$TOTINCR) <- "Family Income"
+label(subdata$HIEDUC) <- "Highest Level of Education Achieved"
+label(subdata$RELRAISD) <- "Religion Raised In"
+label(subdata$ATTND14) <- "Frequency of Church Attendance at 14"
+label(subdata$RELIGION) <- "Current Religious Affiliation"
+label(subdata$EVRMARRY) <- "Ever Been Married"
+label(subdata$ONOWN18) <- "Lived on Their Own Before 18"
+label(subdata$VRY1STAG) <- "Age at First Intercourse"
+label(subdata$topics_discussed_count) <- "How Many Sexual Topics Taught"
+label(subdata$discuss_topic1) <- "Taught How to Say No to Sex"
+label(subdata$discuss_topic2) <- "Taught Methods of Birth Control"
+label(subdata$discuss_topic3) <- "Taught Where to Get Birth Control"
+label(subdata$discuss_topic4) <- "Taught About Sexually Transmitted Diseases"
+label(subdata$discuss_topic5) <- "Taught How to Prevent HIV/AIDS"
+label(subdata$discuss_topic6) <- "Taught How to Use a Condom"
 
 # Actually build the Table 1:
 my_table <- table1(~ AGER + RACE + PARMARR + INTACT18 + TOTINCR + HIEDUC + RELRAISD + ATTND14 + RELIGION + EVRMARRY + ONOWN18 + VRY1STAG + topics_discussed_count + discuss_topic1 +
-discuss_topic2 + discuss_topic3 + discuss_topic4 + discuss_topic5 + discuss_topic6 | discuss_topic8, data = table)
+discuss_topic2 + discuss_topic3 + discuss_topic4 + discuss_topic5 + discuss_topic6 | discuss_topic8, data = subdata, render = rndr, render.continuous=c("Mean (SD)"="MEAN (SD)"))

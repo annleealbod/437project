481ff53c48b6b2f4beff7ccc41176b7f7e1747a4
Anna Bodily <annalallen6@gmail.com>
2024-04-16 13:21:13 -0600

    new table 1

--- Project Background Analysis.R
+++ Project Background Analysis.R
@@ -193,8 +193,80 @@ matched1 <- project_data[c(psmatch1$index.treated, psmatch1$index.control),]
 xvars <- c("RELRAISD", "ATTND14","discuss_topic1",
 "discuss_topic2", "discuss_topic3", "discuss_topic4", "discuss_topic5", "discuss_topic6")
 
-#evaluate matching via table 1
-matchedtab1<-CreateTableOne(vars=xvars, strata ="discuss_topic8", data=matched1, test = FALSE)
+#evaluate matching via table 1 (REMAKING TABLE 1, lots of formatting)
+# Filter out the nevers and refused
+matched1 <- subset(matched1, !(RELRAISD %in% c("Don't know", "Refused")))
+matched1 <- subset(matched1, ATTND14 != "Refused")
+matched1 <- subset(matched1, ATTND14 != "Missing")
+
+matched1$discuss_topic8 <- factor(matched1$discuss_topic8, levels = c(0,1), labels = c("Taught Abstinence", "Not Taught Abstinence"))
+matched1$discuss_topic1 <- as.logical(matched1$discuss_topic1)
+matched1$discuss_topic2 <- as.logical(matched1$discuss_topic2)
+matched1$discuss_topic3 <- as.logical(matched1$discuss_topic3)
+matched1$discuss_topic4 <- as.logical(matched1$discuss_topic4)
+matched1$discuss_topic5 <- as.logical(matched1$discuss_topic5)
+matched1$discuss_topic6 <- as.logical(matched1$discuss_topic6)
+matched1$premarital <- as.logical(matched1$premarital)
+matched1$EVRMARRY <- as.logical(matched1$EVRMARRY == "EVER MARRIED")
+matched1$ONOWN18 <- as.logical(matched1$ONOWN18 == "Yes")
+matched1$PARMARR <- as.logical(matched1$PARMARR == "Yes")
+matched1$INTACT18 <- as.logical(matched1$INTACT18 == "Yes")
+
+
+
+# Reorder certain variable factor levels:
+inc_order <- c("Under $5000", "$5000-$7499", "$7500-$9999", "$10,000-$12,499", "$12,500-$14,999", "$15,000-$19,999", 
+"$20,000-$24,999", "$25,000-$29,999", "$30,000-$34,999", "$35,000-$39,999", "$40,000-$49,999", "$50,000-$59,999", "$60,000-$74,999",
+"$75,000-$99,999", "$100,000 or more")
+matched1$TOTINCR <- factor(matched1$TOTINCR, levels = inc_order)
+
+race_order <- c("WHITE", "BLACK", "OTHER")
+matched1$RACE <- factor(matched1$RACE, levels = race_order)
+levels(matched1$RACE) <- toTitleCase(tolower(levels(matched1$RACE)))
+
+educ_order <- c("9TH GRADE OR LESS", "10TH GRADE", "11TH GRADE", "12TH GRADE, NO DIPLOMA (NOR GED)", "HIGH SCHOOL GRADUATE (DIPLOMA OR GED)",
+"SOME COLLEGE BUT NO DEGREE", "ASSOCIATE DEGREE IN COLLEGE/UNIVERSITY", "BACHELOR'S DEGREE", "MASTER'S DEGREE", "DOCTORATE DEGREE")
+matched1$HIEDUC <- factor(matched1$HIEDUC, levels = educ_order)
+# Rename the levels to lowercase
+levels(matched1$HIEDUC) <- toTitleCase(tolower(levels(matched1$HIEDUC)))
+
+rel_order <- c("Catholic", "Baptist/Southern Baptist", "Methodist, Lutheran, Presbyterian, Episcopal", "Fundamentalist Protestant",
+"Other Protestant denomination","Protestant - No specific denomination", "Other religion", "No religion")
+matched1$RELRAISD <- factor(matched1$RELRAISD, levels = rel_order)
+
+chufreq_order <- c("More than once a week", "Once a week", "2-3 times a month", "Once a month (about 12 times a year)", "3-11 times a year", "Once or twice a year")
+matched1$ATTND14 <- factor(matched1$ATTND14, levels = chufreq_order)
+
+age_order <- c("15 YEARS", "16 YEARS", "17 YEARS", "18 YEARS", "19 YEARS", "20 YEARS", "21 YEARS", "22 YEARS", "23 YEARS", "24 YEARS", "25 YEARS")
+matched1$AGER <- factor(matched1$AGER, levels = age_order)
+levels(matched1$AGER) <- tolower(levels(matched1$AGER))
+
+# Label variables:
+label(matched1$AGER) <- "Age at Time of Survey"
+label(matched1$RACE) <- "Race"
+label(matched1$PARMARR) <- "Parents Married at Birth"
+label(matched1$INTACT18) <- "Lived in Intact Family From 0-18"
+label(matched1$TOTINCR) <- "Family Income"
+label(matched1$HIEDUC) <- "Highest Level of Education Achieved"
+label(matched1$RELRAISD) <- "Religion Raised In"
+label(matched1$ATTND14) <- "Frequency of Church Attendance at 14"
+label(matched1$RELIGION) <- "Current Religious Affiliation"
+label(matched1$EVRMARRY) <- "Have Been Married"
+label(matched1$ONOWN18) <- "Lived on Their Own Before 18"
+label(matched1$VRY1STAG) <- "Age at First Intercourse"
+label(matched1$topics_discussed_count) <- "How Many Sexual Topics Taught"
+label(matched1$discuss_topic1) <- "Taught How to Say No to Sex"
+label(matched1$discuss_topic2) <- "Taught Methods of Birth Control"
+label(matched1$discuss_topic3) <- "Taught Where to Get Birth Control"
+label(matched1$discuss_topic4) <- "Taught About Sexually Transmitted Diseases"
+label(matched1$discuss_topic5) <- "Taught How to Prevent HIV/AIDS"
+label(matched1$discuss_topic6) <- "Taught How to Use a Condom"
+label(matched1$premarital) <- "Had Premarital Sex"
+
+# Actually build the Table 1:
+table_one_matched <- table1(~ AGER + RACE + PARMARR + INTACT18 + TOTINCR + HIEDUC + RELRAISD + ATTND14 + EVRMARRY + ONOWN18 + VRY1STAG + topics_discussed_count + discuss_topic1 +
+discuss_topic2 + discuss_topic3 + discuss_topic4 + discuss_topic5 + discuss_topic6 | discuss_topic8, data = matched1, render = rndr, render.continuous=c("Mean (SD)"="MEAN (SD)"))
+
 print(matchedtab1, smd = TRUE)
 
 
@@ -213,7 +285,6 @@ matched2 <- project_data[c(psmatch2$index.treated, psmatch2$index.control),]
 
 #get standardized differences
 matchedtab2<-CreateTableOne(vars=xvars, strata ="discuss_topic8", data=matched2, test = FALSE)
-print(matchedtab2, smd = TRUE)
 
 #outcome analysis
 y_trt2<-matched2$premarital[matched2$discuss_topic8==1]
